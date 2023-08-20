c69179a0408f07ad7ca7bb5065c73a9371abf165
Isaac Pereira <iespereira@gmail.com>
2023-08-20 12:27:30 -0300

    Incluídas pastas e arquivos de modelo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Importa bibliotecas
+# Importa bibliotecas 
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
